fd044ad609d14fa596d21d67e0b0cb41a02ec82f
Ho Tan Anh <anh152k3@gmail.com>
2023-11-21 10:47:27 +0700

    responsive product, product cart

--- src/Data/data.ts
+++ src/Data/data.ts
@@ -3,7 +3,7 @@ export const dataProduct=[
         id:1,
         imageURL:"https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/v/n/vn_iphone_15_pink_pdp_image_position-1a_pink_color_2.jpg",
         brand: "iphone",
-        title: "iPhone 15 128GB | Chính hãng VN/A",
+        title: "iPhone 15 128GB | Chính hãng VN/A ",
         color:"hồng",
         discountedPrice: 21990000,
         price: 22990000,
