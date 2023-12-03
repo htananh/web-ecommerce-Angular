7dcc41ed76cbfaff7a235f57a40732586c9a6eeb
Ho Tan Anh <anh152k3@gmail.com>
2023-12-04 02:54:24 +0700

    edit for deloy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import SwiperCore,{Navigation,Pagination,EffectCoverflow,Autoplay} from 'swiper' 
+SwiperCore.use([Navigation,Pagination,EffectCoverflow,Autoplay]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
