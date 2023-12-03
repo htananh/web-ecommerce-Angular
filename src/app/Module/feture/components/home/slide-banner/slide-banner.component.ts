e5b81e5ec5e3312e0c104db69a389ac150a0c02b
Ho Tan Anh <anh152k3@gmail.com>
2023-12-04 01:44:25 +0700

    edit slidebanerData

--- src/app/Module/feture/components/home/slide-banner/slide-banner.component.ts
+++ src/app/Module/feture/components/home/slide-banner/slide-banner.component.ts
@@ -13,8 +13,36 @@ export class SlideBannerComponent {
   slideBanner:any;
  
   ngOnInit() {
-    this.slideBanner=SlideBanner;
+    this.slideBanner = SlideBanner;
+  
+    // Kiểm tra dung lượng của ảnh trong SlideBanner
+    this.slideBanner.forEach((image: any) => {
+      this.getImageSize(image.image)
+        .then((sizeInKB) => {
+          console.log(`Dung lượng ảnh: ${sizeInKB} KB`);
+        })
+        .catch((error) => {
+          console.log(`Lỗi khi lấy dung lượng ảnh: ${error}`);
+        });
+    });
   }
+  
+  getImageSize(url: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = url;
+  
+      img.onload = () => {
+        const fileSizeInKB = Math.round((img.src.length / 1024)); // 1 byte = 1024 bytes
+        resolve(fileSizeInKB);
+      };
+  
+      img.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
+  
   
 
   
