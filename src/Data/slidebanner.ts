30387cc2f9063c53e65296765895e92d44171ac3
Ho Tan Anh <anh152k3@gmail.com>
2023-12-04 01:49:32 +0700

    edit slidebanner css

--- src/Data/slidebanner.ts
+++ src/Data/slidebanner.ts
@@ -2,14 +2,14 @@ export const SlideBanner=[
     {    
      image:"https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/09/banner/IP15-720-220-720x220-5.png"
     },
-    {    
-        image:"https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:80/plain/https://dashboard.cellphones.com.vn/storage/sliding_tcl-40-paper-th12.jpg"
-    },
+    // {    
+    //     image:"https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:80/plain/https://dashboard.cellphones.com.vn/storage/sliding_tcl-40-paper-th12.jpg"
+    // },
    
-    // {    
-    //     image:"https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/10/banner/Mua-kem-720-220-720x220-1.png"
-    // },
-    // {    
-    //     image:"https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:80/plain/https://dashboard.cellphones.com.vn/storage/690x300-may-choi-game-ps5-update.png"
-    // },
+    {    
+        image:"https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2023/10/banner/Mua-kem-720-220-720x220-1.png"
+    },
+    {    
+        image:"https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:80/plain/https://dashboard.cellphones.com.vn/storage/690x300-may-choi-game-ps5-update.png"
+    },
 ]
